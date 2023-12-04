c3b0b3f9b00b290ec8639cdef5c0d4a0bc1012c7
Tom Conroy <tconroy@squarespace.com>
2023-12-04 18:05:16 -0500

    day4 pt2 complete

--- 2023/04/day-4.ts
+++ 2023/04/day-4.ts
@@ -1,18 +1,17 @@
 const data = (await (Deno.readTextFile('./day-4.txt'))).trim();
 const lines = data.split('\n');
-const numsRegex = new RegExp('(\\d+)', 'g');
 
 type WinningObj = Record<string, { winningNumbers: Array<number>, myNumbers: Array<number>, myWinningNumbers: Array<number> }>;
 
-function sumPoints(obj: WinningObj) {
-  function doubleNumberNTimes(num: number, N: number) {
-    for (let i = 0; i < N; i++) {
-        num *= 2;
-    }
-    return num;
+function doubleNumberNTimes(num: number, N: number) {
+  for (let i = 0; i < N; i++) {
+      num *= 2;
   }
+  return num;
+}
 
-  return Object.entries(obj).reduce((partialSum, [gameName, entry]) => {
+function sumPoints(obj: WinningObj) {
+  return Object.entries(obj).reduce((partialSum, [_gameName, entry]) => {
     if (entry.myWinningNumbers.length) {
       const increment = doubleNumberNTimes(1, entry.myWinningNumbers.length - 1);
       return partialSum + increment;
@@ -22,15 +21,29 @@ function sumPoints(obj: WinningObj) {
   }, 0);
 }
 
-function getWininngNumbers() {
-  const cardNameRegex = new RegExp(/Card\s*\d+:\s*/);
-  const obj: WinningObj = {};
+function sumCards(obj: WinningObj) {
+  const cardInstances = new Array(Object.entries(obj).length).fill(1);
 
-  lines.forEach(line => {
-    const cardName = cardNameRegex.exec(line)?.[0].trim();
-    if (!cardName) {
-      throw new Error('card name not extracted!');
+  Object.entries(obj).forEach(([_cardName, cardObj], j) => {
+    const matchCount = cardObj.myWinningNumbers.length;
+
+    if (matchCount) {
+      for (let i = j + 1; i <= j + matchCount; i++) {
+        cardInstances[i] += cardInstances[j];
+      }
     }
+  });
+
+  return cardInstances.reduce((acc, v) => acc + v, 0);
+}
+
+function getWininngNumbers(obj: WinningObj = {}) {
+  const cardNameRegex = new RegExp(/Card\s*\d+:\s*/);
+
+  lines.forEach((line) => {
+    const cardName = cardNameRegex.exec(line)?.[0].trim();
+    if (!cardName) throw new Error('card name not extracted!');
+
     const [winningNumbers, myNumbers] = line.substring(cardName.length)
       .split('|')
       .map(s => s.trim())
@@ -44,20 +57,23 @@ function getWininngNumbers() {
         });
       });
 
-    const myWinningNumbers = myNumbers.filter(n => winningNumbers.includes(n));
-    
-    obj[cardName] = { winningNumbers, myNumbers, myWinningNumbers, };
+    obj[cardName] = {
+      winningNumbers,
+      myNumbers,
+      myWinningNumbers: myNumbers.filter(n => winningNumbers.includes(n)),
+    };
   });
 
   return obj;
 }
 
 function main() {
+  const winningNumbers = getWininngNumbers();
   console.log({
-    // expected: 
-    part1: sumPoints(getWininngNumbers())
-    // expected: 
-    // part2: '',
+    // expected: 26218
+    part1: sumPoints(winningNumbers),
+    // expected: 9997537
+    part2: sumCards(winningNumbers),
   });
 }
 
